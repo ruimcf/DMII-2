d73f96c83a95ec9aeb4803c942dcb58462069426
Rui Fonseca <ruimcf95@gmail.com>
2017-05-03 20:39:13 +0100

    Add getDetails

--- main.R
+++ main.R
@@ -26,4 +26,43 @@ searchTitle <- function(query, max=200){
   return(movieList)
 }
 
+getTitle <- function(movieID, page=NULL){
+  if(is.null(page)){
+    page <- read_html(str_interp("http://www.imdb.com/title/${movieID}"))
+  }
+  return(html_text(html_nodes(page, "#title-overview-widget h1")))
+}
+
+getYear <- function(movieID, page=NULL){
+  if(is.null(page)){
+    page <- read_html(str_interp("http://www.imdb.com/title/${movieID}"))
+  }
+  return(page %>% html_nodes("#titleYear a") %>% html_text() %>% as.numeric())
+}
+
+getCast <- function(movieID, page=NULL){
+  if(is.null(page)){
+    page <- read_html(str_interp("http://www.imdb.com/title/${movieID}"))
+  }
+  return(page %>% html_nodes("#titleCast .itemprop span") %>% html_text())
+}
+
+
+getDirector <- function(movieID, page=NULL){
+  if(is.null(page)){
+    page <- read_html(str_interp("http://www.imdb.com/title/${movieID}"))
+  }
+  return(html_nodes(page, ".credit_summary_item .itemprop")[1] %>% html_text())
+}
+
+getDetails <- function(movieID){
+  link <- str_interp("http://www.imdb.com/title/${movieID}")
+  page <- read_html(link)
+  description <- c(link=link, title=getTitle(movieID,page), year=getYear(movieID,page),
+                   director=getDirector(movieID,page), cast=getCast(movieID,page))
+  return(description)
+}
+
 movieList <- searchTitle("Kill Bill")
+details <- getDetails(movieList[1])
+details
